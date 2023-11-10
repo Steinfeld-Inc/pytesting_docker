bc81ddcb47996d642da03a6b9eb0c83be19ed767
fabianstf <55556613+fabianstf@users.noreply.github.com>
2023-11-10 15:58:14 +0100

    Update test_a.py

--- test_a.py
+++ test_a.py
@@ -18,5 +18,5 @@ def test_answer():
         version = subprocess.run('pip freeze', shell=True, encoding='utf-8', stdout=subprocess.PIPE)
         for line in version.stdout.split('\n'):
             f.write(line + "\n")
-    assert inc(21) == 22
+    assert inc(22) == 23
     sleep(100)
