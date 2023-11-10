4ef9da1fbb1434b11d510c3b2310b535e670635c
fabianstf <55556613+fabianstf@users.noreply.github.com>
2023-11-10 14:36:52 +0100

    Update test_a.py

--- test_a.py
+++ test_a.py
@@ -6,8 +6,9 @@ def inc(x):
 
 
 def test_answer():
+    version = subprocess.run("python --version", shell=True)
     with open("/tmp/test_on_server.txt", "w") as f:
-        f.write(str(subprocess.run("python --version", shell=True)))
+        f.write(str(version.stdout)"\n"))
         f.write(str(subprocess.run("pwd", shell=True)))
         f.write(str(subprocess.run("pip freeze", shell=True)))
     assert inc(20) == 21
